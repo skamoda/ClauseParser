5a95c57d199629e669adb9b2b351a94ce6a7fdda
Sebastian Kamoda <seba.kamoda@gmail.com>
2019-01-09 00:06:35 +0100

    Adding application template

---
+++ ClauseParser/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ClauseParser.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Parse(string text)
+        {
+            ViewBag.text = text;
+
+            return View("Index");
+        }
+    }
+}
