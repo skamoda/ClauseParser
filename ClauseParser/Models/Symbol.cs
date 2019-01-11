e40114fbabda2f2c7e59994dc93fc43b08ffdee4
Sebastian Kamoda <seba.kamoda@gmail.com>
2019-01-11 22:29:10 +0100

    Creating init classes Symbol and Parser

---
+++ ClauseParser/Models/Symbol.cs
@@ -0,0 +1,18 @@
+﻿namespace ClauseParser.Models
+{
+    public class Symbol
+    {
+        public Symbol Parent { get; set; }
+        public string Name { get; set; }
+        public int Priority { get; set; }
+        public int ChildrenCount { get; set; }
+
+        public Symbol()
+        {
+            Parent = null;
+            Name = null;
+            Priority = 0;
+            ChildrenCount = 0;
+        }
+    }
+}
